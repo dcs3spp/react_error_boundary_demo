60101171ee166fa53f53ec1ed33ed11d1409de1a
dcs3spp <roughlea-music@outlook.com>
2019-11-03 14:25:47 +0000

    linting: remove unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
-import todoApp from './redux/reducers'
 import App from './App'
 
 import store from './redux/store';
