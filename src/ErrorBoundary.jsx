c8458ef0fe2dc3ce77986fc0ec7b88d79e759ebf
dcs3spp <roughlea-music@outlook.com>
2019-11-03 14:34:00 +0000

    linting: remove unused import

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
-import { UnregisterCallback } from 'history';
 
 export class ErrorBoundary extends Component {
   static homePath = '/';
